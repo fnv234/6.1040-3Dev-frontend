09ee11a8430abb2a616aea2b6520607238db3683
Francesca Venditti <135988334+fnv234@users.noreply.github.com>
2025-12-01 11:43:01 -0500

    updating meta env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,6 +3,8 @@
 interface ImportMetaEnv {
   readonly DEV: boolean;
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_SENDGRID_API_KEY: string;
+  readonly VITE_SENDER_EMAIL: string;
 }
 
 interface ImportMeta {
